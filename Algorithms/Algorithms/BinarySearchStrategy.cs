7dc7c1eaa0659643b9ee3bddf1ce727482ef3424
eevos <46033079+eevos@users.noreply.github.com>
2021-11-30 21:26:45 +0100

    Added BinarySearch

---
+++ Algorithms/Algorithms/BinarySearchStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.Algorithms;
+
+public class BinarySearchStrategy<T>
+{
+    public int search(T[] array, T item)
+    {
+        var result = array.Length;
+        return result;
+    }
+}
---
+++ Tests/UnitTests/AdpBinarySearchStrategyTest.cs
@@ -0,0 +1,18 @@
+﻿using Algorithms.Algorithms;
+using Xunit;
+
+namespace Tests.UnitTests;
+
+public class AdpBinarySearchStrategyTest
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(1,2,3,4)]
+    public void ReturnItem(params int[] values) //int -> T, opgelost door mischa
+    {
+        var item = 1; 
+        var expected = values.Length; 
+        var sut = new BinarySearchStrategy<int>();
+        Assert.Equal(expected, sut.search(values, item));
+    }
+}
