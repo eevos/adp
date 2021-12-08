f456c188631adbe4eaf263a4c2020e5d7701eb83
thullner <m.s.thullner@gmail.com>
2021-12-08 16:53:45 +0100

    Add stack

---
+++ Algorithms/DataStructures/AdpStack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+namespace Algorithms.DataStructures;
+
+public class AdpStack<T> : IEnumerable<T>
+{
+    private T[] _items;
+    private int _size;
+    
+    public AdpStack(int capacity)
+    {
+        _items = new T[capacity];
+        _size = 0;
+    }
+    
+    public int Count
+    {
+        get { return _size; }
+    }
+    
+    public void Push(T item)
+    {
+        if (_size == _items.Length)
+        {
+            throw new StackOverflowException();
+        }
+        
+        _items[_size++] = item;
+    }
+    
+    public T Pop()
+    {
+        if (_size == 0)
+        {
+            throw new InvalidOperationException();
+        }
+        
+        return _items[--_size];
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return ((IEnumerable<T>)_items).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Tests/UnitTests/AdpStackTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Algorithms.DataStructures;
+using Tests.DataSets;
+using Xunit;
+
+namespace Tests.UnitTests;
+
+public class AdpStackTest : BaseListTest
+{
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Push_CheckIfItemIsAdded<T>(T[] values)
+    {
+        var stack = new AdpStack<T>(values.Length);
+        foreach (var value in values)
+            stack.Push(value);
+
+        Assert.Equal(values, stack.ToArray());
+    }
+    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Push_CheckIfCountIsIncreased<T>(T[] values)
+    {
+        var stack = new AdpStack<T>(values.Length);
+        foreach (var value in values)
+            stack.Push(value);
+
+        Assert.Equal(values.Length, stack.Count);
+    }
+    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Push_CheckIfStackOverflowExceptionIsThrown<T>(T[] values)
+    {
+        var stack = new AdpStack<T>(values.Length);
+        foreach (var value in values)
+            stack.Push(value);
+        
+        var valueToAdd = GetValueForType<T>();
+
+        Assert.Throws<StackOverflowException>(() => stack.Push(valueToAdd));
+    }
+    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Pop_CheckIfLastItemIsReturned<T>(T[] values)
+    {
+        if (AssertIndexOutOfRangeWhenEmptyArray(values)) return;
+        var stack = new AdpStack<T>(values.Length);
+        foreach (var value in values)
+            stack.Push(value);
+
+        var lastItem = stack.Pop();
+        Assert.Equal(values.Last(), lastItem);
+    }
+    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Pop_CheckIfItemIsRemoved<T>(T[] values)
+    {
+        if (AssertIndexOutOfRangeWhenEmptyArray(values)) return;
+        var stack = new AdpStack<T>(values.Length);
+        foreach (var value in values)
+            stack.Push(value);
+
+        var valueToRemove = stack.Pop();
+        Assert.Equal(values.Last(), valueToRemove);
+    }
+    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Pop_CheckIfCountIsDecreased<T>(T[] values)
+    {
+        if (AssertIndexOutOfRangeWhenEmptyArray(values)) return;
+        var stack = new AdpStack<T>(values.Length);
+        foreach (var value in values)
+            stack.Push(value);
+
+        stack.Pop();
+        Assert.Equal(values.Length - 1, stack.Count);
+    }
+    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Pop_CheckIfInvalidOperationExceptionIsThrown<T>(T[] values)
+    {
+        var stack = new AdpStack<T>(values.Length);
+        Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
+}
