d8b6c335c119d50e1d2c7d99d0704ef7657af8f0
eevos <46033079+eevos@users.noreply.github.com>
2021-12-10 16:22:53 +0100

    MergeSort with <T> implemented
    Debug tests, start looking at successful tests

--- Algorithms/Algorithms/MergeSortStrategy.cs
+++ Algorithms/Algorithms/MergeSortStrategy.cs
@@ -1,23 +1,88 @@
-﻿namespace Algorithms.Algorithms;
+﻿using System.Collections;
+using System.Linq.Expressions;
 
-public class MergeSortStrategy//<T>where T : IComparable
+namespace Algorithms.Algorithms;
+
+public class MergeSortStrategy<T>//where T : IComparable
 {
     public MergeSortStrategy()
     {
     }
-public int[] MergeSort(int[] array)
+// public int[] MergeSort(int[] array)
+//     {
+//         int[] left;
+//         int[] right;
+//         int[] result = new int[array.Length];
+//         if (array.Length <= 1) return array;
+//         var mid = array.Length / 2;
+//         left = new int[mid];
+//         
+//         if (array.Length % 2 == 0)
+//             right = new int[mid];
+//         else
+//             right = new int[mid + 1];
+//         for (var i = 0; i < mid; i++)
+//             left[i] = array[i];
+//         var x = 0;
+//         for (var i = mid; i < array.Length; i++)
+//         {
+//             right[x] = array[i];
+//             x++;
+//         }
+//         left = MergeSort(left);
+//         right = MergeSort(right);
+//         result = Merge(left, right);
+//         return result;
+//     }
+//     
+//     private static int[] Merge(int[] left, int[] right)
+//     {
+//         int[] result = new int[right.Length + left.Length];
+//         var iResult = 0;
+//         var iRight = 0;
+//         var iLeft = 0;
+//         while (iLeft < left.Length && iRight < right.Length)
+//         {
+//             if (left[iLeft].CompareTo(right[iRight]) >= 0)
+//             {
+//                 result[iResult] = right[iRight];
+//                 iRight++;
+//             }
+//             else
+//             {
+//                 result[iResult] = left[iLeft];
+//                 iLeft++;
+//             }
+//             iResult++;
+//         }
+//         while (iLeft < left.Length)
+//         {
+//             result[iResult] = left[iLeft];
+//             iLeft++;
+//             iResult++;
+//         }
+//         while (iRight < right.Length)
+//         {
+//             result[iResult] = right[iRight];
+//             iRight++;
+//             iResult++;
+//         }
+//         return result;
+//     }
+//     
+    public T[] MergeSort(T[] array)
     {
-        int[] left;
-        int[] right;
-        int[] result = new int[array.Length];
+        T[] left;
+        T[] right;
+        T[] result = new T[array.Length];
         if (array.Length <= 1) return array;
         var mid = array.Length / 2;
-        left = new int[mid];
+        left = new T[mid];
         
         if (array.Length % 2 == 0)
-            right = new int[mid];
+            right = new T[mid];
         else
-            right = new int[mid + 1];
+            right = new T[mid + 1];
         for (var i = 0; i < mid; i++)
             left[i] = array[i];
         var x = 0;
@@ -32,15 +97,17 @@ public int[] MergeSort(int[] array)
         return result;
     }
     
-    private static int[] Merge(int[] left, int[] right)
+    private static T[] Merge(T[] left, T[] right)
     {
-        int[] result = new int[right.Length + left.Length];
+        T[] result = new T[right.Length + left.Length];
         var iResult = 0;
         var iRight = 0;
         var iLeft = 0;
+        var comparer = Comparer<T>.Default;
+
         while (iLeft < left.Length && iRight < right.Length)
         {
-            if (left[iLeft].CompareTo(right[iRight]) >= 0)
+            if (comparer.Compare(left[iLeft], right[iRight]) >= 0)
             {
                 result[iResult] = right[iRight];
                 iRight++;
@@ -67,127 +134,66 @@ public int[] MergeSort(int[] array)
         return result;
     }
     
-    // public T[] MergeSort(T[] array)
-    // {
-    //     T[] left;
-    //     T[] right;
-    //     T[] result = new T[array.Length];
-    //     if (array.Length <= 1) return array;
-    //     var mid = array.Length / 2;
-    //     left = new T[mid];
-    //     
-    //     if (array.Length % 2 == 0)
-    //         right = new T[mid];
-    //     else
-    //         right = new T[mid + 1];
-    //     for (var i = 0; i < mid; i++)
-    //         left[i] = array[i];
-    //     var x = 0;
-    //     for (var i = mid; i < array.Length; i++)
-    //     {
-    //         right[x] = array[i];
-    //         x++;
-    //     }
-    //     left = MergeSort(left);
-    //     right = MergeSort(right);
-    //     result = Merge(left, right);
-    //     return result;
-    // }
-    //
-    // private static T[] Merge(T[] left, T[] right)
-    // {
-    //     T[] result = new T[right.Length + left.Length];
-    //     var iResult = 0;
-    //     var iRight = 0;
-    //     var iLeft = 0;
-    //     while (iLeft < left.Length && iRight < right.Length)
-    //     {
-    //         if (left[iLeft].CompareTo(right[iRight]) >= 0)
-    //         {
-    //             result[iResult] = right[iRight];
-    //             iRight++;
-    //         }
-    //         else
-    //         {
-    //             result[iResult] = left[iLeft];
-    //             iLeft++;
-    //         }
-    //         iResult++;
-    //     }
-    //     while (iLeft < left.Length)
-    //     {
-    //         result[iResult] = left[iLeft];
-    //         iLeft++;
-    //         iResult++;
-    //     }
-    //     while (iRight < right.Length)
-    //     {
-    //         result[iResult] = right[iRight];
-    //         iRight++;
-    //         iResult++;
-    //     }
-    //     return result;
-    // }
+    public List<T> MergeSort(List<T> array)
+    {
+        List<T> left;
+        List<T> right;
+        List<T> result = new List<T>();
+        if (array.Count <= 1) return array;
+        var mid = array.Count / 2;
+        left = new List<T>();
+        
+        if (array.Count % 2 == 0)
+            right = new List<T>();
+        else
+            right = new List<T>();
+        for (var i = 0; i < mid; i++)
+            left[i] = array[i];
+        var x = 0;
+        for (var i = mid; i < array.Count; i++)
+        {
+            right[x] = array[i];
+            x++;
+        }
+        left = MergeSort(left);
+        right = MergeSort(right);
+        result = Merge(left, right);
+        return result;
+    }
     
-    // public List<T> MergeSort(List<T> array)
-    // {
-    //     List<T> left;
-    //     List<T> right;
-    //     List<T> result = new List<T>();
-    //     if (array.Count <= 1) return array;
-    //     var mid = array.Count / 2;
-    //     left = new List<T>();
-    //     
-    //     if (array.Count % 2 == 0)
-    //         right = new List<T>();
-    //     else
-    //         right = new List<T>();
-    //     for (var i = 0; i < mid; i++)
-    //         left[i] = array[i];
-    //     var x = 0;
-    //     for (var i = mid; i < array.Count; i++)
-    //     {
-    //         right[x] = array[i];
-    //         x++;
-    //     }
-    //     left = MergeSort(left);
-    //     right = MergeSort(right);
-    //     result = Merge(left, right);
-    //     return result;
-    // }
-    //
-    // private List<T> Merge(List<T> left, List<T> right)
-    // {
-    //     List<T> result = new List<T>();
-    //     var iResult = 0;
-    //     var iRight = 0;
-    //     var iLeft = 0;
-    //     while (iLeft < left.Count && iRight < right.Count)
-    //     {
-    //         if (left[iLeft].CompareTo(right[iRight]) >= 0)
-    //         {
-    //             result[iResult] = right[iRight];
-    //             iRight++;
-    //         }
-    //         else
-    //         {
-    //             result[iResult] = left[iLeft];
-    //             iLeft++;
-    //         }
-    //         iResult++;
-    //     }
-    //     while (iLeft < left.Count)
-    //     {
-    //         result[iResult] = left[iLeft];
-    //         iLeft++;
-    //         iResult++;
-    //     }
-    //     while (iRight < right.Count)
-    //     {
-    //         result[iResult] = right[iRight];
-    //         iRight++;
-    //         iResult++;
-    //     }
-    //     return result;
-    // }
+    private List<T> Merge(List<T> left, List<T> right)
+    {
+        List<T> result = new List<T>();
+        var iResult = 0;
+        var iRight = 0;
+        var iLeft = 0;
+        var comparer = Comparer<T>.Default;
+        while (iLeft < left.Count && iRight < right.Count)
+        {
+            if (comparer.Compare(left[iLeft], right[iRight]) >= 0)
+            {
+                result[iResult] = right[iRight];
+                iRight++;
+            }
+            else
+            {
+                result[iResult] = left[iLeft];
+                iLeft++;
+            }
+            iResult++;
+        }
+        while (iLeft < left.Count)
+        {
+            result[iResult] = left[iLeft];
+            iLeft++;
+            iResult++;
+        }
+        while (iRight < right.Count)
+        {
+            result[iResult] = right[iRight];
+            iRight++;
+            iResult++;
+        }
+        return result;
+    }
 }
--- Tests/UnitTests/MergeSortTest.cs
+++ Tests/UnitTests/MergeSortTest.cs
@@ -8,24 +8,23 @@ namespace Tests.UnitTests;
 
 public class MergeSortTest
 {
-    [Theory]
-    [InlineData(2, 3, 4, 5, 6, 7, 8, 9, 1)]
-    public void MergeSort_ShouldReturnSortedList_WithIntArray(params int[] values)
-    {
-        var expected = new int[]{1,2,3,4,5,6,7,8,9};
-        var sut = new MergeSortStrategy();
-        Assert.Equal(expected, sut.MergeSort(values));
-    }
-    
     // [Theory]
-    // [ClassData(typeof(DataSetLoader<DsSortDto>))]
-    // public void MergeSort_ShouldReturnSortedList_WithIntList<T>(T[] values) where T : IComparable
-    // {   
-        // var valuesList = new List<T>(values);
-        // var expected = new List<T>(values);
-        // expected.Sort();
-        // var sut = new MergeSortStrategy<T>();
-        // Assert.Equal(expected, sut.MergeSort(valuesList));
+    // [InlineData(2, 3, 4, 5, 6, 7, 8, 9, 1)]
+    // public void MergeSort_ShouldReturnSortedList_WithIntArray(params int[] values)
+    // {
+    //     var expected = new int[]{1,2,3,4,5,6,7,8,9};
+    //     var sut = new MergeSortStrategy();
+    //     Assert.Equal(expected, sut.MergeSort(values));
     // }
-    
+    //
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void MergeSort_ShouldReturnSortedList_WithIntList<T>(T[] values)
+    {   
+        var valuesList = new List<T>(values);
+        var expected = new List<T>(values);
+        expected.Sort();
+        var sut = new MergeSortStrategy<T>();
+        Assert.Equal(expected, sut.MergeSort(valuesList));
+    }
 }
