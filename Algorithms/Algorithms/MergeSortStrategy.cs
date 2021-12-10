74f555a10366e73245ad517eddcee30d0c055ff7
eevos <46033079+eevos@users.noreply.github.com>
2021-12-10 13:15:05 +0100

    Added List<int> method and test
    Outcommented

--- Algorithms/Algorithms/MergeSortStrategy.cs
+++ Algorithms/Algorithms/MergeSortStrategy.cs
@@ -67,4 +67,66 @@ public class MergeSortStrategy
         }
         return result;
     }
+    //
+    // public List<int> MergeSort(List<int> array)
+    // {
+    //     List<int> left;
+    //     List<int> right;
+    //     List<int> result = new List<int>();
+    //     if (array.Count <= 1) return array;
+    //     int midPoint = array.Count / 2;
+    //     left = new List<int>();
+    //     
+    //     if (array.Count % 2 == 0)
+    //         right = new List<int>();
+    //     else
+    //         right = new List<int>();
+    //     for (int i = 0; i < midPoint; i++)
+    //         left[i] = array[i];
+    //     int x = 0;
+    //     for (int i = midPoint; i < array.Count; i++)
+    //     {
+    //         right[x] = array[i];
+    //         x++;
+    //     }
+    //     left = MergeSort(left);
+    //     right = MergeSort(right);
+    //     result = Merge(left, right);
+    //     return result;
+    // }
+    //
+    // private List<int> Merge(List<int> left, List<int> right)
+    // {
+    //     List<int> result = new List<int>();
+    //     var iResult = 0;
+    //     var iRight = 0;
+    //     var iLeft = 0;
+    //     while (iLeft < left.Count && iRight < right.Count)
+    //     {
+    //         if (left[iLeft] > right[iRight])
+    //         {
+    //             result[iResult] = right[iRight];
+    //             iRight++;
+    //         }
+    //         else
+    //         {
+    //             result[iResult] = left[iLeft];
+    //             iLeft++;
+    //         }
+    //         iResult++;
+    //     }
+    //     while (iLeft < left.Count)
+    //     {
+    //         result[iResult] = left[iLeft];
+    //         iLeft++;
+    //         iResult++;
+    //     }
+    //     while (iRight < right.Count)
+    //     {
+    //         result[iResult] = right[iRight];
+    //         iRight++;
+    //         iResult++;
+    //     }
+    //     return result;
+    // }
 }
--- Tests/UnitTests/MergeSortTest.cs
+++ Tests/UnitTests/MergeSortTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Algorithms.Algorithms;
 using Tests.DataSets;
 using Xunit;
@@ -9,13 +10,21 @@ public class MergeSortTest
 {
     [Theory]
     [InlineData(2, 3, 4, 5, 6, 7, 8, 9, 1)]
-    // [InlineData(1, 2, 3, 4, 5, 6, 7, 8, 9)]
-    public void MergeSort_ShouldReturnSortedList_WithNumbers(params int[] values)
+    public void MergeSort_ShouldReturnSortedList_WithIntArray(params int[] values)
     {
         var expected = new int[]{1,2,3,4,5,6,7,8,9};
         var sut = new MergeSortStrategy();
         Assert.Equal(expected, sut.MergeSort(values));
     }
-    
+    // [Theory]
+    // [MemberData(nameof(List<int>),2, 3, 4, 5, 6, 7, 8, 9, 1)]
+    // // [InlineData(1, 2, 3, 4, 5, 6, 7, 8, 9)]
+    // public void MergeSort_ShouldReturnSortedList_WithIntList(List<int> values)
+    // {   
+    //     var valuesList = new List<int> {2, 3, 4, 5, 6, 7, 8, 9, 1};
+    //     var expected = new List<int>{1,2,3,4,5,6,7,8,9};
+    //     var sut = new MergeSortStrategy();
+    //     Assert.Equal(expected, sut.MergeSort(valuesList));
+    // }
     
 }
