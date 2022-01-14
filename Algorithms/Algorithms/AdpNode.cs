a6c6837f322a2c8cc0d77512ecd1f12b03cbf70a
eevos <46033079+eevos@users.noreply.github.com>
2022-01-14 16:53:39 +0100

    AdpNode and AdpTree added tests

---
+++ Algorithms/Algorithms/AdpNode.cs
@@ -0,0 +1,72 @@
+﻿namespace Algorithms.Algorithms;
+
+
+public class AdpNode<T>
+{
+    private int _depth;
+    private AdpNode<T>? _childRight;
+    private AdpNode<T>? _childLeft;
+    private AdpNode<T>? _parent;
+    private T? _data;
+
+    public AdpNode(T? data = default, 
+        AdpNode<T>? parent = null, AdpNode<T>? childLeft = null,
+        AdpNode<T>? childRight = null, int depth = default)
+    {
+        this._data = data;
+        this._parent = parent;
+        this._childLeft = childLeft;
+        this._childRight = childRight;
+        this._depth = depth;
+    }
+
+    public void SetDepth(int depth)
+    {
+        this._depth = depth;
+    }
+
+    public int GetDepth()
+    {
+        return this._depth;
+    }
+
+    public T? GetData()
+    {
+        return this._data;
+    }
+
+    public void SetData(T data)
+    {
+        this._data = data;
+    }
+
+    public AdpNode<T>? GetParent()
+    {
+        return this._parent;
+    }
+
+    public void SetParent(AdpNode<T> parent)
+    {
+        this._parent = parent;
+    }
+
+    public AdpNode<T>? GetLeftChild()
+    {
+        return this._childLeft;
+    }
+
+    public void SetLeftChild(AdpNode<T> childLeft)
+    {
+        this._childLeft = childLeft;
+    }
+
+    public AdpNode<T>? GetRightChild()
+    {
+        return this._childRight;
+    }
+
+    public void SetRightChild(AdpNode<T> childRight)
+    {
+        this._childRight = childRight;
+    }
+}
--- Tests/UnitTests/AdpAVLTreeTest.cs
+++ Tests/UnitTests/AdpAVLTreeTest.cs
@@ -10,22 +10,24 @@ public class AdpAVLTreeTest
     [ClassData(typeof(DataSetLoader<DsSortDto>))]
     public void SetDepth_ShouldReturnDepth<T>(T[] values)
     {
-        var insertedDepth = 1;
-        var expected = insertedDepth;
-        var sut = new AdpTree<T>.AdpNode<T>();
-        sut.SetDepth(insertedDepth);
-        Assert.Equal(expected, sut.GetDepth());
+        var expected = 1;
+        var node = new AdpNode<T>();
+        var sut = new AdpTree<T>(node);
+        sut.ListRoot.SetDepth(1);
+        
+        Assert.Equal(expected, sut.ListRoot.GetDepth());
+    }    
+    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void InsertNode_ShouldReturnDepth<T>(T[] values)
+    {
+        var insertedData = values[0];
+        var sut = new AdpTree<T>(new AdpNode<T>(insertedData,null,null,null,1));
+        var expected = 1;
+        Assert.Equal(expected, sut.ListRoot.GetDepth());
     }
     
     
-    
-    
-    // [Theory]
-    // [ClassData(typeof(DataSetLoader<DsSortDto>))]
-    // public void InsertShouldMakeNode<T>(T[] values)
-    // {
-    //     
-    //     Assert.Equal(expected, actual);
-    // }
     
 }
