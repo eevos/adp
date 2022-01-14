e6b19edd6bba21800a1969c50e911bcba6560407
eevos <46033079+eevos@users.noreply.github.com>
2022-01-14 16:24:56 +0100

    Added AdpTree and AdpTreeTest

---
+++ Tests/UnitTests/AdpAVLTreeTest.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms.Algorithms;
+using Tests.DataSets;
+using Xunit;
+
+namespace Tests.UnitTests;
+
+public class AdpAVLTreeTest
+{
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void SetDepth_ShouldReturnDepth<T>(T[] values)
+    {
+        var insertedDepth = 1;
+        var expected = insertedDepth;
+        var sut = new AdpTree<T>.AdpNode<T>();
+        sut.SetDepth(insertedDepth);
+        Assert.Equal(expected, sut.GetDepth());
+    }
+    
+    
+    
+    
+    // [Theory]
+    // [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    // public void InsertShouldMakeNode<T>(T[] values)
+    // {
+    //     
+    //     Assert.Equal(expected, actual);
+    // }
+    
+}
