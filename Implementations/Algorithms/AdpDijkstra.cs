ebe176576bea53a62284105abc50f7c0d5bb885a
thullner <m.s.thullner@gmail.com>
2022-01-23 17:22:27 +0100

    Add Dijkstra

---
+++ Implementations/Algorithms/AdpDijkstra.cs
@@ -0,0 +1,68 @@
+﻿using Implementations.DataStructures;
+
+namespace Implementations.Algorithms;
+
+public class AdpDijkstra
+{
+    public static void FindShortestPath(AdpGraph graph, int startIndex, DijkstraDistance[] distances)
+    {
+        distances[startIndex].Distance = 0;
+        for (var i = 0; i < distances.Length; i++)
+        {
+            var minVertexIndex = MinVertex(graph, distances);
+            distances[minVertexIndex].IsVisited = true;
+            
+            if (distances[minVertexIndex].Distance == int.MaxValue) { return; }
+            var neighbours = graph.Neighbours(minVertexIndex);
+            foreach (var neighbourIndex in neighbours)
+            {
+                var neighbourDistance = distances[minVertexIndex].Distance + graph.Weight(minVertexIndex, neighbourIndex);
+                if (neighbourDistance < distances[neighbourIndex].Distance)
+                {
+                    distances[neighbourIndex].Distance = neighbourDistance;
+                    distances[neighbourIndex].PreviousVertexIndex = minVertexIndex;
+                }
+            }
+        }
+    }
+
+    private static int MinVertex(AdpGraph graph, DijkstraDistance[] distances)
+    {
+        var vertexIndex = 0;
+        for (var i = 0; i < distances.Length; i++)
+        {
+            if (distances[i].IsVisited) continue;
+            vertexIndex = i;
+            break;
+        }
+
+        for (var i = 0; i < graph.Count(); i++)
+        {
+            if (distances[i].IsVisited) continue;
+            if (distances[i].Distance < distances[vertexIndex].Distance)
+            {
+                vertexIndex = i;
+            }
+        }
+
+        return vertexIndex;
+    }
+}
+
+public class DijkstraDistance
+{
+    public int Distance { get; set; } = int.MaxValue;
+    public int PreviousVertexIndex { get; set; } = -1;
+    public bool IsVisited { get; set; } = false;
+
+    public DijkstraDistance()
+    {
+    }
+
+    public DijkstraDistance(int distance, int previousVertexIndex, bool isVisited)
+    {
+        Distance = distance;
+        PreviousVertexIndex = previousVertexIndex;
+        IsVisited = isVisited;
+    }
+}
---
+++ Tests/UnitTests/AdpDijkstraTest.cs
@@ -0,0 +1,59 @@
+﻿using Implementations.Algorithms;
+using Implementations.DataSets;
+using Implementations.DataStructures;
+using Xunit;
+
+namespace Tests.UnitTests;
+
+public class AdpDijkstraTest
+{
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsGraphMatrixDto>))]
+    public void DijkstraTest(int[,] values)
+    {
+        var graph = new AdpGraph(values);
+        var distances = new DijkstraDistance[graph.Count()];
+        for (var i = 0; i < distances.Length; i++)
+        {
+            distances[i] = new DijkstraDistance();
+        }
+        
+        AdpDijkstra.FindShortestPath(graph, 0, distances);
+        
+        var isWeighted = values.GetLength(0) == 5;
+        var expectedDistances = isWeighted ? getExpectedWeightedDijkstraDistances() : getExpectedDijkstraDistances();
+        for (var i = 0; i < distances.Length; i++)
+        {
+            Assert.Equal(expectedDistances[i].Distance, distances[i].Distance);
+            Assert.Equal(expectedDistances[i].PreviousVertexIndex, distances[i].PreviousVertexIndex);
+            Assert.Equal(expectedDistances[i].IsVisited, distances[i].IsVisited);
+        }
+    }
+
+    private DijkstraDistance[] getExpectedWeightedDijkstraDistances()
+    {
+        return new DijkstraDistance[]
+        {
+            new(0, -1, true),
+            new(99, 0, true),
+            new(50, 0, true),
+            new(149, 2, true),
+            new(149, 1, true),
+        };
+    }
+
+    private DijkstraDistance[] getExpectedDijkstraDistances()
+    {
+            return new DijkstraDistance[]
+        {
+            new(0, -1, true),
+            new(1, 0, true),
+            new(1, 0, true),
+            new(2, 1, true),
+            new(2, 2, true),
+            new(3, 4, true),
+            new(4, 5, true),
+        };
+    }
+    
+}
