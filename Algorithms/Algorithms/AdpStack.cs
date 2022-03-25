81cbd4edf60274352bf766908626087292e733ec
eevos <46033079+eevos@users.noreply.github.com>
2022-03-25 16:15:09 +0100

    AdpStack added methods and tests

---
+++ Algorithms/Algorithms/AdpStack.cs
@@ -0,0 +1,65 @@
+﻿namespace Algorithms.Algorithms;
+
+public class AdpStack<T>
+{
+    private List<T> _items;
+    private int _top;
+    private int _size;
+    
+    public AdpStack()
+    {
+        _items = new List<T>();
+        _top = 0;
+        _size = 0;
+    }
+    public AdpStack(T[] values)
+    {
+        _items = new List<T>();
+        _top = 0;
+        _size = 0;
+        foreach (var value in values)
+        {
+            Push(value);
+        }
+    }
+    public T Pop()
+    {
+        if (_top < 1)
+        {
+            throw new Exception("Stack is empty");
+        }
+        var result = _items[_top-1];
+        _top--;   
+        return result;
+    }
+    public void Push(T item)
+    {
+        _top++;
+        _size++;
+        _items.Add(item);
+    }
+
+    public void Clear()
+    {
+        _items.Clear();
+        _top = 0;
+    }
+
+    public int CountSize()
+    {
+        if (_top < 1)
+        {
+            throw new Exception("Stack is empty");
+        }
+        return _size;
+    }
+
+    public int FindTop()
+    {       
+        if (_top < 1)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+        return _top;
+    }
+}
---
+++ Tests/UnitTests/AdpStackTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Algorithms.Algorithms;
+using Tests.DataSets;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Tests.UnitTests;
+
+public class AdpStackTest
+{
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Pop1_TopLowerThen_Size<T>(T[] values)
+    {        
+        var stack = new AdpStack<T>(values);
+        var top = stack.FindTop();
+
+        var thing = stack.Pop();
+        if (stack.FindTop() == 0)
+        {
+            
+            var excStack = new AdpStack<T>();
+            var text = "Stack is empty";
+            // var ex = Assert.Throws<Exception>(() => (Exception)excStack.FindTop());
+            // Assert.Throws<Exception>(()=>excStack.FindTop());
+        }
+        Assert.Equal(top - 1,stack.FindTop());
+    }    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void PushValues_CountSizeShouldReturn_Size<T>(T[] values)
+    {
+        var stack = new AdpStack<T>(values);
+        Assert.Equal(values.Length,stack.CountSize());
+    }
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Constructor_ShouldReturnNotNull<T>(T[] values)
+    {
+        var stack = new AdpStack<T>(values);
+        Assert.NotNull(stack);
+    }
+}
