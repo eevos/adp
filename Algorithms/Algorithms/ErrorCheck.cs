a6c79953e2c0cb8ded884d8fe12c063c73b0f3fb
eevos <46033079+eevos@users.noreply.github.com>
2022-03-29 10:41:58 +0200

    Create ErrorCheck.cs

---
+++ Algorithms/Algorithms/ErrorCheck.cs
@@ -0,0 +1,37 @@
+﻿// namespace Algorithms.Algorithms;
+//
+// public class ErrorCheck<T>
+// {
+//     private T _items;
+//
+//
+//     public ErrorCheck(T items)
+//     {
+//         _items = items;
+//     }
+//
+//     private void ErrorCheck(LinkedListNode<T> node)
+//     {
+//         if (_items.First.Value == null || node.Value == null || _items.Count == 0)
+//         {
+//             throw new Exception("LinkedList is empty");
+//         }
+//
+//         if (_items.Count > 2500)
+//         {
+//             throw new Exception("LinkedList is too big for memory");
+//         }
+//     }
+//     private void ErrorCheck()
+//     {
+//         if (_items.First.Value == null || _items.Count == 0)
+//         {
+//             throw new Exception("LinkedList is empty");
+//         }
+//
+//         if (_items.Count > 2500)
+//         {
+//             throw new Exception("LinkedList is too big for memory");
+//         }
+//     }
+// }
