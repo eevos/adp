0d95c31ebfa2603da608fad36ba101b018e99982
eevos <46033079+eevos@users.noreply.github.com>
2022-03-29 10:41:41 +0200

    AdpDeque finished with tests

--- Algorithms/Algorithms/AdpDeque.cs
+++ Algorithms/Algorithms/AdpDeque.cs
@@ -2,14 +2,112 @@
 
 public class AdpDeque<T>
 {
-    private List<T> _items;
-    private int _size;
+    private LinkedList<T> _items;
+
+    public AdpDeque(T[] items)
+    {
+        _items = new LinkedList<T>(items);
+    }
 
     public AdpDeque()
     {
-        var _items = new List<T>();
-        _size = 0;
-
+        _items = new LinkedList<T>();
     }
-    
+
+    public int Count()
+    {
+        var count = 0;
+        foreach (var item in _items)
+        {
+            count++;
+        }
+
+        return count;
+    }
+
+    public void Clear()
+    {
+        _items.Clear();
+    }
+
+    public void AddLeft(T itemToAdd)
+    {
+        _items.AddFirst(itemToAdd);
+    }
+
+    public void AddRight(T itemToAdd)
+    {
+        _items.AddLast(itemToAdd);
+    }
+
+    public T GetLeft()
+    {
+        if (_items.First == null)
+            ErrorCheck();
+        else
+            ErrorCheck(_items.First);
+
+        return _items.First.Value;
+    }
+
+    public T GetRight()
+    {
+        if (_items.First == null)
+            ErrorCheck();
+        else
+            ErrorCheck(_items.First);
+        return _items.Last.Value;
+    }
+
+    public void RemoveRight()
+    {
+        if (_items.First == null)
+        {
+            ErrorCheck();
+        }
+        else
+        {
+            ErrorCheck(_items.First);
+        }
+        _items.RemoveLast();
+    }
+
+    public void RemoveLeft()
+    {
+        if (_items.First == null)
+        {
+            ErrorCheck();
+        }
+        else
+        {
+            ErrorCheck(_items.First);
+        }
+        _items.RemoveFirst();
+    }
+
+    private void ErrorCheck(LinkedListNode<T> node)
+    {
+        if (_items.First == null || node.Value == null || _items.Count == 0)
+        {
+            throw new Exception("LinkedList is empty");
+        }
+
+        if (_items.Count > 2500)
+        {
+            throw new Exception("LinkedList is too big for memory");
+        }
+    }
+
+    private void ErrorCheck()
+    {
+        if (_items.First == null || _items.Count == 0)
+        {
+            throw new Exception("LinkedList is empty");
+        }
+
+        if (_items.Count > 2500)
+        {
+            throw new Exception("LinkedList is too big for memory");
+        }
+    }
 }
--- Tests/UnitTests/AdpDequeTest.cs
+++ Tests/UnitTests/AdpDequeTest.cs
@@ -1,6 +1,109 @@
-﻿namespace Tests.UnitTests;
+﻿using System;
+using Algorithms.Algorithms;
+using Tests.DataSets;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Tests.UnitTests;
 
 public class AdpDequeTest
 {
-    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void RemoveLeftRight_FirstAndLast_AreNotFound<T>(T[] values)
+    {
+        if (values == null || values.Length == 0 || values[0] == null|| values.Length > 2500)
+        {
+            var deque = new AdpDeque<T>(values);
+
+            Assert.Throws<Exception>(() => deque.RemoveLeft());
+        }
+        else
+        {
+            var deque = new AdpDeque<T>(values);
+            var valueToAdd = GetValueForType<T>();
+            
+            deque.AddLeft(valueToAdd);
+            deque.AddRight(valueToAdd);
+            var leftValue = deque.GetLeft();
+            var rightValue = deque.GetRight();
+            deque.RemoveLeft();
+            deque.RemoveRight();
+            
+            Assert.NotEqual(leftValue, deque.GetLeft());
+            Assert.NotEqual(rightValue, deque.GetRight());
+        }
+    }
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void AddLeftRight_FirstAndLast_AreFound<T>(T[] values)
+    {
+        if (values == null || values.Length == 0 || values[0] == null|| values.Length > 2500)
+        {
+            var deque = new AdpDeque<T>(values);
+
+            Assert.Throws<Exception>(() => deque.GetLeft());
+        }
+        else
+        {
+            var deque = new AdpDeque<T>(values);
+            
+            var valueToAdd = GetValueForType<T>();
+            deque.AddLeft(valueToAdd);
+            deque.AddRight(valueToAdd);
+
+            Assert.Equal(valueToAdd, deque.GetLeft());
+            Assert.Equal(valueToAdd, deque.GetRight());
+        }
+    }
+
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void GetLeft_ReturnsFirst<T>(T[] values)
+    {
+        if (values == null || values.Length == 0 || values[0] == null || values.Length > 2500)
+        {
+            var deque = new AdpDeque<T>(values);
+
+            Assert.Throws<Exception>(() => deque.GetLeft());
+        }
+        else
+        {
+            var deque = new AdpDeque<T>(values);
+
+            Assert.Equal(values[0], deque.GetLeft());
+        }
+    }
+
+[Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Clear_ReturnsCorrectNumber<T>(T[] values)
+    {
+        var deque = new AdpDeque<T>(values);
+        deque.Clear();
+        Assert.Equal(0, deque.Count());
+    }
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Count_ReturnsCorrectNumber<T>(T[] values)
+    {
+        var deque = new AdpDeque<T>(values);
+        var count = deque.Count();
+        Assert.Equal(values.Length, count);
+    }
+    private static T GetValueForType<T>()
+    {
+        // switch with type
+        return typeof(T).Name switch
+        {
+            "Int32" => (T) Convert.ChangeType(992147, typeof(T)),
+            "String" => (T) Convert.ChangeType("notInArray", typeof(T)),
+            "Boolean" => (T) Convert.ChangeType(true, typeof(T)),
+            "Single" => (T) Convert.ChangeType(913133139, typeof(T)),
+            "Object" => (T) Convert.ChangeType("notInArray", typeof(T)),
+            "Nullable`1" => (T) Convert.ChangeType(93131434, Nullable.GetUnderlyingType(typeof(T))),
+            _ => throw new Exception("Type not supported")
+        };
+    }
 }
