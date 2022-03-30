0cd3a435de2de68981644a414edd7c9d34f00c84
eevos <46033079+eevos@users.noreply.github.com>
2022-03-30 14:33:45 +0200

    SortTests added QuickSortTest

--- Tests/UnitTests/SortTests.cs
+++ Tests/UnitTests/SortTests.cs
@@ -1,4 +1,5 @@
 ﻿namespace Tests.UnitTests;
+
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -8,18 +9,48 @@ using Xunit;
 
 public class SortTests
 {
-    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void QuickSort_ShouldReturnSortedList<T>(T[] values)
+    {
+        var sut = new QuickSortStrategy<T>();
+
+        var expected = values;
+        if (typeof(T).Name == "Object")
+        {
+            Assert.Throws<InvalidOperationException>(() => Array.Sort(expected));
+            return;
+        }
+
+        if (values.Length < 1)
+        {
+            Assert.Throws<InvalidOperationException>(() => sut.QuickSort(values, 0, values.Length - 1));
+            return;
+        }
+
+        Array.Sort(expected);
+        var actual = sut.QuickSort(values, 0, values.Length - 1);
+
+        Assert.Equal(expected, actual);
+    }
+
     [Theory]
     [ClassData(typeof(DataSetLoader<DsSortDto>))]
     public void SelectionSort_ShouldReturnEqual<T>(T[] values)
     {
         var sut = new AdpSelectionSort<T>();
-        
+        if (typeof(T).Name == "Object")
+        {
+            Assert.Throws<InvalidOperationException>(() => Array.Sort(values));
+            return;
+        }
+
         var expected = new List<T>(values);
-        expected.Sort();        
-        
+        expected.Sort();
+
         Assert.Equal(expected, sut.Sort(values));
     }
+
     [Theory]
     [ClassData(typeof(DataSetLoader<DsSortDto>))]
     public void InsertionSortRecursive_ShouldReturnSortedList<T>(T[] values)
@@ -30,30 +61,32 @@ public class SortTests
             Assert.Throws<InvalidOperationException>(() => Array.Sort(expected));
             return;
         }
+
         Array.Sort(expected);
-        
+
         var insertionSortStrategy = new InsertionSortStrategy<T>();
         var actual = insertionSortStrategy.RecursiveSort(values, values.Length);
 
         Assert.Equal(expected, actual);
     }
-    
+
     [Theory]
     [ClassData(typeof(DataSetLoader<DsSortDto>))]
     public void InsertionSortRecursiveForLoop_ShouldReturnSortedList<T>(T[] values)
     {
         var expected = new List<T>(values);
-        
+
         var insertionSortStrategy = new InsertionSortStrategy<T>();
         var actual = insertionSortStrategy.RecursiveSortForLoop(values, values.Length);
-        
+
         if (typeof(T).Name == "Object")
         {
             Assert.Throws<InvalidOperationException>(() => expected.Sort());
             return;
         }
+
         expected.Sort();
-        
+
         Assert.Equal(expected, actual);
     }
 }
