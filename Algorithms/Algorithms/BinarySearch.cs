b0e45be57e453e35b6e64eab0e2156e8db1b6ded
eevos <46033079+eevos@users.noreply.github.com>
2022-03-30 11:10:32 +0200

    AdpBinarySearch complete with tests

---
+++ Algorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿namespace Algorithms.Algorithms;
+
+public class BinarySearch<T>
+{
+    public bool Search(T[] array, T item)
+    {
+        var low = 0;
+        var high = array.Length - 1;
+        
+        while (low <= high)
+        {
+            var middle = (low + high) / 2;
+            var comparer = Comparer<T>.Default;
+            if (comparer.Compare(item, array[middle]) == 0)
+            {
+                return true;
+            }
+            if (comparer.Compare(item, array[middle]) < 0)
+            {
+                high = middle - 1;
+            }
+            else
+            {
+                low = middle + 1;
+            }
+        }
+        return false;
+    }
+}
---
+++ Tests/UnitTests/AdpBinarySearchTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Algorithms.Algorithms;
+using Tests.DataSets;
+using Xunit;
+
+namespace Tests.UnitTests;
+
+public class AdpBinarySearchStrategyTest : TestHelper
+{
+
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void Search_ShouldReturnTrue<T>(T[] values)
+    {
+        var sut = new BinarySearch<T>();
+        Array.Sort(values); // array must be sorted for Binary Search
+
+        if (values.Length == 0) // you can't search for empty
+        {
+            return;
+        }
+
+        Random random = new Random();
+        var i = random.Next(0, values.Length - 1);
+        var expected = values[i];
+        
+        Assert.True(sut.Search(values,expected));
+    }    
+    [Theory]
+    [ClassData(typeof(DataSetLoader<DsSortDto>))]
+    public void SearchNonExistent_ShouldReturnFalse<T>(T[] values)
+    {
+        var sut = new BinarySearch<T>();
+        Array.Sort(values); 
+
+        if (values.Length == 0) 
+        {
+            return;
+        }
+
+        var expected = GetValueForType<T>(); // this number can't exist
+        
+        Assert.False(sut.Search(values,expected));
+    }
+}
